e8b1fe734dbadd432a35fb588dad1efdbb1b4b39
capziell <nrkelly@outlook.com>
2017-05-18 13:32:41 -0600

    roguelike 4/14

---
+++ Learning/Assets/scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public Text scoreText;
+
+    public Text winText;
+
+    private int score;
+
+    private Rigidbody playerRigidbody;
+    
+    void Start() //first frame script is active
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        score = 0;
+    }
+
+    void Update() //before rendering frames
+    {
+        
+    }
+
+    void FixedUpdate() //before rendering physics
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        playerRigidbody.AddForce(moveHorizontal * speed, 0, moveVertical * speed);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            scoreText.text = "SCORE: " + score.ToString();
+            if (score >= 12)
+            {
+                winText.text = "YOU WIN!";
+            }
+        }
+    }
+
+}
