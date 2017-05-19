f5f3881257d2ca2b3b3a91f891736aea262be3cc
capziell <nrkelly@outlook.com>
2017-05-19 13:52:01 -0600

    game complete

---
+++ 2d roguelike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;
+    public float turnDelay = 0.1f;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 1 ;
+
+
+    private Text levelText;
+    private GameObject levelImage;
+
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;
+
+
+	// Use this for initialization
+	void Awake ()
+	{
+        
+	    if (instance == null)
+	    {
+	        instance = this;
+	    }
+        else if (instance != this)
+	    {
+	        Destroy(gameObject);
+	    }
+
+        DontDestroyOnLoad(gameObject);
+	    enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+	    InitGame();
+	}
+
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+
+    private void InitGame()
+    {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+    public void AddEnemyToList(Enemy enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (playersTurn || enemiesMoving)
+	    {
+	        return;
+	    }
+	    StartCoroutine(MoveEnemies());
+	}
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+
+    }
+}
---
+++ 2d roguelike/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class MovingObject : MonoBehaviour
+{
+
+    public float moveTime = 0.1f;
+    public LayerMask BlockingLayerMask;
+
+    private BoxCollider2D boxCollider2D;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+
+	// Use this for initialization
+	protected virtual void Start ()
+	{
+	    boxCollider2D = GetComponent<BoxCollider2D>();
+	    rb2D = GetComponent<Rigidbody2D>();
+	    inverseMoveTime = 1f / moveTime;
+
+	}
+
+    protected bool Move(int xDirection, int yDirection, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDirection, yDirection);
+        boxCollider2D.enabled = false;
+        hit = Physics2D.Linecast(start, end, BlockingLayerMask);
+        boxCollider2D.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+        return false;
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        float squareRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (squareRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            squareRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove<T>(int xDirection, int yDirection) where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDirection, yDirection, out hit);
+
+        if (hit.transform == null)
+        {
+            return;
+        }
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+        {
+            OnCantMove(hitComponent);
+        }
+    }
+
+    protected abstract void OnCantMove<T>(T component) where T : Component;
+
+
+}
